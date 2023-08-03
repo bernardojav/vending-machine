a0f218d88cf3753090d709d47e42c92984db367f
Tiaro-hub <130729389+Tiaro-hub@users.noreply.github.com>
2023-08-03 21:02:00 +0800

    Update mainGUI.java

--- mainGUI.java
+++ mainGUI.java
@@ -3,15 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
-/**
- *
- * @author molin
- */
 public class mainGUI extends javax.swing.JFrame {
     
     private static mainGUI instance; // Static instance variable
