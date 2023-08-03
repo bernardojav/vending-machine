7b046e29074946ff6d5b27f69c06cac504d9f124
Tiaro-hub <130729389+Tiaro-hub@users.noreply.github.com>
2023-08-03 21:04:04 +0800

    Update regVendo.java

--- regVendo.java
+++ regVendo.java
@@ -3,10 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 
-/**
- *
- * @author molin
- */
+
 public class regVendo extends javax.swing.JFrame {
 
     /**
